37da114d02d8edfc669c6f1b65fc1c6f8c00bdda
Serge Belyshev <belyshev@depni.sinp.msu.ru>
2021-11-15 18:52:52 +0300

    muqss: Backport 42a20f86dc19 (sched: Add wrapper for get_wchan() to keep task blocked)

--- kernel/sched/MuQSS.c
+++ kernel/sched/MuQSS.c
@@ -8326,3 +8326,22 @@ void call_trace_sched_update_nr_running(struct rq *rq, int count)
 #ifdef CONFIG_RCU_TORTURE_TEST
 int sysctl_sched_rt_runtime;
 #endif
+
+unsigned long get_wchan(struct task_struct *p)
+{
+	unsigned long ip = 0;
+	unsigned int state;
+
+	if (!p || p == current)
+		return 0;
+
+	/* Only get wchan if task is blocked and we can keep it that way. */
+	raw_spin_lock_irq(&p->pi_lock);
+	state = READ_ONCE(p->__state);
+	smp_rmb(); /* see try_to_wake_up() */
+	if (state != TASK_RUNNING && state != TASK_WAKING && !p->on_rq)
+		ip = __get_wchan(p);
+	raw_spin_unlock_irq(&p->pi_lock);
+
+	return ip;
+}
