d3e21fccd80183327b46adb5ed09b93478731301
Serge Belyshev <belyshev@depni.sinp.msu.ru>
2022-08-23 14:01:04 +0300

    muqss: Backport 9c2136be0878 (sched/tracing: Append prev_state to tp args instead)

--- kernel/sched/MuQSS.c
+++ kernel/sched/MuQSS.c
@@ -4349,7 +4349,7 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 
 		psi_sched_switch(prev, next, !task_on_rq_queued(prev));
 
-		trace_sched_switch(sched_mode & SM_MASK_PREEMPT, prev_state, prev, next);
+		trace_sched_switch(sched_mode & SM_MASK_PREEMPT, prev, next, prev_state);
 		context_switch(rq, prev, next); /* unlocks the rq */
 	} else {
 		check_siblings(rq);
